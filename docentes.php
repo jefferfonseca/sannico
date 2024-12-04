24098fb131020e1e61613b244cd2cc1b481805ee
Ing. Jeffer Fonseca <jefferfonseca@gmail.com>
2024-12-04 15:11:33 -0500

    todo

--- docentes.php
+++ docentes.php
@@ -1,12 +1,12 @@
 <?php $menu = "Institución";
 include('includes/header.php'); ?>
 
-<div class="section p-0 m-0">
-<h3 class="center white-text"><b>DOCENTES</b></h3>
-<!--------------------- Docentes Directivos ------------------------------>
-<h4 class="center white-text">Directivos Docentes</h4>
-        <div class="row">
-            <br>
+<div class="section ">
+    <h3 class="center yellow-text p-0 m-0 pt"><b>DOCENTES</b></h3>
+    <!--------------------- Docentes Directivos ------------------------------>
+    <h4 class="center yellow-text">Directivos Docentes</h4>
+    <div class="contenedor">
+        <div class="row cent">
             <!-- Leer archivo csv con php para crear dodentes-->
             <?php
             $linea = 0;
@@ -20,13 +20,26 @@ include('includes/header.php'); ?>
                     // Recorremos las columnas de esa fila
                     if ($num >= 6) { // Asegurarnos de que hay suficientes columnas
             ?>
-                        <div class="col s12 m6 center">
-                            <img src="./images/<?php echo htmlspecialchars($docente[0]) ?>.png" alt="" width="auto" height="140">
-                            <h5 class="green-text "><b><?php echo htmlspecialchars($docente[1] . ' ' . $docente[2]) ?></b></h5>
-                            <p class="black-text center" style="margin: 0;"><b>Celular: </b><a href="https://wa.me/+57<?php echo htmlspecialchars($docente[3]) ?>" target="_blank">+57<?php echo htmlspecialchars($docente[3]) ?></a></p>
-                            <p class="black-text center" style="margin: 0;"><b>E-mail: </b><?php echo htmlspecialchars($docente[4]) ?></p>
-                            <p class="black-text center" style="margin: 0;"><b>Cargo: </b><?php echo htmlspecialchars($docente[5]) ?></p>
-                            <br><br><br>
+                        <div class="col s12 m4 container">
+                            <div class="row valign-wrapper">
+                                <div class="col s4">
+                                    <div class="image-container">
+                                        <!-- Rectángulo verde detrás de la imagen -->
+                                        <div class="green-rectangle borde-dorado"></div>
+                                        <img src="./images/docentes/<?php echo htmlspecialchars($docente[0]) ?>.png" alt="" width="100%">
+                                    </div>
+                                </div>
+                                <div class="col s8">
+                                    <!-- Nombres -->
+                                    <h5 class="yellow-text left-align  m-0"><b><?php echo htmlspecialchars($docente[1]) ?></b></h5>
+                                    <h6 class="yellow-text left-align apellidos m-0"><b><?php echo htmlspecialchars($docente[2]) ?></b></h6>
+                                    <!-- Cargo -->
+                                    <p class="white-text cargo" style="margin: 0;"><b><?php echo htmlspecialchars($docente[5]) ?></b></p>
+                                    <!-- Correo -->
+                                    <p class="white-text email" style="margin: 0;"><b>E-mail: </b><?php echo htmlspecialchars($docente[4]) ?></p>
+
+                                </div>
+                            </div>
                         </div>
             <?php
                     }
@@ -38,168 +51,196 @@ include('includes/header.php'); ?>
             }
             ?>
         </div>
-        <br>
     </div>
-</div>
 
-<!--------------------- Docentes Secundaria ------------------------------>
-<div class="section">
-    <h3 class="center white-text">Docentes Secundaria</h3>
-</div>
-<br>
-
-<div class="row">
-    <div class="col s12 opacity">
+    <!--------------------- Docentes Secundaria ------------------------------>
+    <h4 class="center yellow-text">Docentes Secundaria</h4>
+    <div class="contenedor">
         <div class="row">
             <br>
+            <!-- Leer archivo csv con php para crear dodentes-->
             <?php
             $linea = 0;
+            // Verificar si el archivo existe y se puede abrir
             $archivo = "./lista_docentes/secundaria.csv";
             if (($handle = fopen($archivo, "r")) !== FALSE) {
+                // Recorremos fila por fila con el while
                 while (($docente = fgetcsv($handle, 1000, ",")) !== FALSE) {
                     $num = count($docente);
                     $linea++;
-                    if ($num >= 6) {
+                    // Recorremos las columnas de esa fila
+                    if ($num >= 6) { // Asegurarnos de que hay suficientes columnas
             ?>
-                        <div class="col s12 m6 l4 center">
-                            <img src="./images/<?php echo htmlspecialchars($docente[0]) ?>.png" alt="" width="auto" height="140">
-                            <h5 class="green-text "><b><?php echo htmlspecialchars($docente[1] . ' ' . $docente[2]) ?></b></h5>
-                            <p class="black-text center" style="margin: 0;"><b>Celular: </b><a href="https://wa.me/+57<?php echo htmlspecialchars($docente[3]) ?>" target="_blank">+57<?php echo htmlspecialchars($docente[3]) ?></a></p>
-                            <p class="black-text center" style="margin: 0;"><b>E-mail: </b><?php echo htmlspecialchars($docente[4]) ?></p>
-                            <p class="black-text center" style="margin: 0;"><b>Área: </b><?php echo htmlspecialchars($docente[5]) ?></p>
-                            <br><br><br>
+                        <div class="col s12 m4">
+                            <div class="row  valign-wrapper">
+                                <div class="col s4">
+                                    <div class="image-container">
+                                        <!-- Rectángulo verde detrás de la imagen -->
+                                        <div class="green-rectangle borde-dorado"></div>
+                                        <img src="./images/docentes/<?php echo htmlspecialchars($docente[0]) ?>.png" alt="" width="100%">
+                                    </div>
+                                </div>
+                                <div class="col s8">
+                                    <!-- Nombres -->
+                                    <h5 class="yellow-text left-align truncate m-0"><b><?php echo htmlspecialchars($docente[1]) ?></b></h5>
+                                    <h6 class="yellow-text left-align apellidos m-0"><b><?php echo htmlspecialchars($docente[2]) ?></b></h6>
+                                    <!-- Cargo -->
+                                    <p class="white-text cargo" style="margin: 0;"><b><?php echo htmlspecialchars($docente[5]) ?></b></p>
+                                    <!-- Correo -->
+                                    <p class="white-text email" style="margin: 0;"><b>E-mail: </b><?php echo htmlspecialchars($docente[4]) ?></p>
+
+                                </div>
+                            </div>
                         </div>
             <?php
                     }
                 }
+                // Cerramos el archivo
                 fclose($handle);
             } else {
                 echo "<p>No se pudo abrir el archivo $archivo.</p>";
             }
             ?>
         </div>
-        <br>
     </div>
-</div>
-
-<!--------------------- Docentes Primaria ------------------------------>
-<div class="section">
-    <h3 class="center white-text">Docentes Primaria</h3>
-</div>
-<br>
-
-<div class="row">
-    <div class="col s12 opacity">
-    <div class="row">
+    <!--------------------- Docentes Primaria ------------------------------>
+    <h4 class="center yellow-text">Docentes Primaria</h4>
+    <div class="contenedor">
+        <div class="row ">
             <br>
+            <!-- Leer archivo csv con php para crear dodentes-->
             <?php
             $linea = 0;
+            // Verificar si el archivo existe y se puede abrir
             $archivo = "./lista_docentes/primaria.csv";
             if (($handle = fopen($archivo, "r")) !== FALSE) {
+                // Recorremos fila por fila con el while
                 while (($docente = fgetcsv($handle, 1000, ",")) !== FALSE) {
                     $num = count($docente);
                     $linea++;
-                    if ($num >= 6) {
+                    // Recorremos las columnas de esa fila
+                    if ($num >= 6) { // Asegurarnos de que hay suficientes columnas
             ?>
-                        <div class="col s12 m6 l4 center">
-                            <img src="./images/<?php echo htmlspecialchars($docente[0]) ?>.png" alt="" width="auto" height="140">
-                            <h5 class="green-text "><b><?php echo htmlspecialchars($docente[1] . ' ' . $docente[2]) ?></b></h5>
-                            <p class="black-text center" style="margin: 0;"><b>Celular: </b><a href="https://wa.me/+57<?php echo htmlspecialchars($docente[3]) ?>" target="_blank">+57<?php echo htmlspecialchars($docente[3]) ?></a></p>
-                            <p class="black-text center" style="margin: 0;"><b>E-mail: </b><?php echo htmlspecialchars($docente[4]) ?></p>
-                            <p class="black-text center" style="margin: 0;"><b>Área: </b><?php echo htmlspecialchars($docente[5]) ?></p>
-                            <br><br><br>
+                        <div class="col s12 m4">
+                            <div class="row  valign-wrapper">
+                                <div class="col s4">
+                                    <div class="image-container">
+                                        <!-- Rectángulo verde detrás de la imagen -->
+                                        <div class="green-rectangle borde-dorado"></div>
+                                        <img src="./images/docentes/<?php echo htmlspecialchars($docente[0]) ?>.png" alt="" width="100%">
+                                    </div>
+                                </div>
+                                <div class="col s8">
+                                    <!-- Nombres -->
+                                    <h5 class="yellow-text left-align truncate m-0"><b><?php echo htmlspecialchars($docente[1]) ?></b></h5>
+                                    <h6 class="yellow-text left-align apellidos m-0"><b><?php echo htmlspecialchars($docente[2]) ?></b></h6>
+                                    <!-- Cargo -->
+                                    <p class="white-text cargo" style="margin: 0;"><b><?php echo htmlspecialchars($docente[5]) ?></b></p>
+                                    <!-- Correo -->
+                                    <p class="white-text email" style="margin: 0;"><b>E-mail: </b><?php echo htmlspecialchars($docente[4]) ?></p>
+
+                                </div>
+                            </div>
                         </div>
             <?php
                     }
                 }
+                // Cerramos el archivo
                 fclose($handle);
             } else {
                 echo "<p>No se pudo abrir el archivo $archivo.</p>";
             }
             ?>
         </div>
-        <br>
     </div>
-</div>
-
-<!--------------------- Docentes Sedes ------------------------------>
-<div class="section">
-    <h3 class="center white-text">Docentes Sedes</h3>
-</div>
-<br>
-
-<div class="row">
-    <div class="col s12 opacity">
-    <div class="row">
+    <!--------------------- Docentes Sedes ------------------------------>
+    <h4 class="center yellow-text">Docentes Primaria</h4>
+    <div class="contenedor">
+        <div class="row ">
             <br>
+            <!-- Leer archivo csv con php para crear dodentes-->
             <?php
             $linea = 0;
+            // Verificar si el archivo existe y se puede abrir
             $archivo = "./lista_docentes/sedes.csv";
             if (($handle = fopen($archivo, "r")) !== FALSE) {
+                // Recorremos fila por fila con el while
                 while (($docente = fgetcsv($handle, 1000, ",")) !== FALSE) {
                     $num = count($docente);
                     $linea++;
-                    if ($num >= 6) {
+                    // Recorremos las columnas de esa fila
+                    if ($num >= 6) { // Asegurarnos de que hay suficientes columnas
             ?>
-                        <div class="col s12 m6 l3 center">
-                            <img src="./images/<?php echo htmlspecialchars($docente[0]) ?>.png" alt="" width="auto" height="140">
-                            <h5 class="green-text "><b><?php echo htmlspecialchars($docente[1] . ' ' . $docente[2]) ?></b></h5>
-                            <p class="black-text center" style="margin: 0;"><b>Celular: </b><a href="https://wa.me/+57<?php echo htmlspecialchars($docente[3]) ?>" target="_blank">+57<?php echo htmlspecialchars($docente[3]) ?></a></p>
-                            <p class="black-text center" style="margin: 0;"><b>E-mail: </b><?php echo htmlspecialchars($docente[4]) ?></p>
-                            <p class="black-text center" style="margin: 0;"><b>Área: </b><?php echo htmlspecialchars($docente[5]) ?></p>
-                            <br><br><br>
+                        <div class="col s12 m4">
+                            <div class="row  valign-wrapper">
+                                <div class="col s4">
+                                    <div class="image-container">
+                                        <!-- Rectángulo verde detrás de la imagen -->
+                                        <div class="green-rectangle borde-dorado"></div>
+                                        <img src="./images/docentes/<?php echo htmlspecialchars($docente[0]) ?>.png" alt="" width="100%">
+                                    </div>
+                                </div>
+                                <div class="col s8">
+                                    <!-- Nombres -->
+                                    <h5 class="yellow-text left-align truncate m-0"><b><?php echo htmlspecialchars($docente[1]) ?></b></h5>
+                                    <h6 class="yellow-text left-align apellidos m-0"><b><?php echo htmlspecialchars($docente[2]) ?></b></h6>
+                                    <!-- Cargo -->
+                                    <p class="white-text cargo" style="margin: 0;"><b><?php echo htmlspecialchars($docente[5]) ?></b></p>
+                                    <!-- Correo -->
+                                    <p class="white-text email" style="margin: 0;"><b>E-mail: </b><?php echo htmlspecialchars($docente[4]) ?></p>
+
+                                </div>
+                            </div>
                         </div>
             <?php
                     }
                 }
+                // Cerramos el archivo
                 fclose($handle);
             } else {
                 echo "<p>No se pudo abrir el archivo $archivo.</p>";
             }
             ?>
         </div>
-        <br>
     </div>
-</div>
 
-
-<script>
-document.addEventListener("DOMContentLoaded", function() {
-        document.body.style.backgroundImage = "url('./images/parallax/14.jpg')";
-    });
-
-    // JavaScript para cambiar el fondo dependiendo de la sección visible
-    document.addEventListener("scroll", function() {
-        const sections = document.querySelectorAll(".section");
-        const scrollPosition = window.scrollY;
-
-        sections.forEach((section, index) => {
-            const sectionTop = section.offsetTop - 20;
-            const sectionHeight = section.offsetHeight;
-
-            if (
-                scrollPosition >= sectionTop &&
-                scrollPosition < sectionTop + sectionHeight
-            ) {
-                switch (index) {
-                    case 0:
-                        document.body.style.backgroundImage = "url('./images/parallax/14.jpg')";
-                        break;
-                    case 1:
-                        document.body.style.backgroundImage = "url('./images/parallax/10.jpg')";
-                        break;
-                    case 2:
-                        document.body.style.backgroundImage = "url('./images/parallax/17.jpg')";
-                        break;
-                    case 3:
-                        document.body.style.backgroundImage = "url('./images/parallax/6.jpg')";
-                        break;
-                    default:
-                        break;
-                }
-            }
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            document.body.style.backgroundImage = "url('./images/parallax/14.jpg')";
         });
-    });
-</script>
 
-<?php include('includes/footer.php'); ?>
+        // JavaScript para cambiar el fondo dependiendo de la sección visible
+        document.addEventListener("scroll", function() {
+            const sections = document.querySelectorAll(".section");
+            const scrollPosition = window.scrollY;
+
+            sections.forEach((section, index) => {
+                const sectionTop = section.offsetTop - 20;
+                const sectionHeight = section.offsetHeight;
+
+                if (
+                    scrollPosition >= sectionTop &&
+                    scrollPosition < sectionTop + sectionHeight
+                ) {
+                    switch (index) {
+                        case 0:
+                            document.body.style.backgroundImage = "url('./images/parallax/14.jpg')";
+                            break;
+                        case 1:
+                            document.body.style.backgroundImage = "url('./images/parallax/10.jpg')";
+                            break;
+                        case 2:
+                            document.body.style.backgroundImage = "url('./images/parallax/17.jpg')";
+                            break;
+                        case 3:
+                            document.body.style.backgroundImage = "url('./images/parallax/6.jpg')";
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            });
+        });
+    </script>
+
+    <?php include('includes/footer.php'); ?>
--- sedes.php
+++ sedes.php
@@ -1,46 +1,47 @@
-<?php $menu="Institución"; include('includes/header.php');?>
+<?php $menu = "Institución";
+include('includes/header.php'); ?>
 
 <!--------------------- Titulo ------------------------------>
-<div class="section pt">
-   <h3 class="center yellow-text p-0 m-0"><b>SEDES</b></h3>
-<!--------------------- Sedes Rurales ------------------------------>
-   <h4 class="center yellow-text">Sedes Rurales</h4>
-<br>
- <div class="row">
-       <div class="col s12">
-           <div class="row">
-           <br>
-               <!--- Sede Central --->
-               <div class="col s12 m6 center ">
-                   <img src="./images/hombre.png" alt="" width= auto height= 140 >
-                   <h4 class="yellow-text "><b>Sede Siderúrgica</b></h4>
-                   <p class="green-text center" style="margin:5px 0;"><b>Vereda El Arenal</b></p>
-               </div>
-              
-               <!--- Sede Siderurjica --->
-               <div class="col s12 m6 center ">
-                   <img src="./images/mujer.png" alt="" width= auto height= 140 >
-                   <h4 class="yellow-text "><b>Sede Central</b></h4>
-                   <p class="green-text center" style="margin:5px 0;"><b>Vereda San Nicolás</b></p>
-               </div>
+<div class="section">
+    <h3 class="center yellow-text p-0 m-0 pt"><b>SEDES</b></h3>
+    <!--------------------- Sedes Rurales ------------------------------>
+    <h4 class="center yellow-text">Sedes Rurales</h4>
+    <br>
+    <div class="row">
+        <div class="col s12">
+            <div class="row">
+                <br>
+                <!--- Sede Central --->
+                <div class="col s12 m6 center ">
+                    <img src="./images/hombre.png" alt="" width=auto height=140>
+                    <h4 class="yellow-text "><b>Sede Siderúrgica</b></h4>
+                    <p class="green-text center" style="margin:5px 0;"><b>Vereda El Arenal</b></p>
+                </div>
 
-               <!--- Sede La Bolsa --->
-               <div class="col s12 m6 center ">
-                   <img src="./images/mujer.png" alt="" width= auto height= 140 >
-                   <h4 class="yellow-text "><b>Sede Salvial</b></h4>
-                   <p class="green-text center" style="margin:5px 0;"><b>Vereda Salvial</b></p>
-               </div>
+                <!--- Sede Siderurjica --->
+                <div class="col s12 m6 center ">
+                    <img src="./images/mujer.png" alt="" width=auto height=140>
+                    <h4 class="yellow-text "><b>Sede Central</b></h4>
+                    <p class="green-text center" style="margin:5px 0;"><b>Vereda San Nicolás</b></p>
+                </div>
 
-               <!--- Sede El Rosal --->
-               <div class="col s12 m6 center ">
-                   <img src="./images/mujer.png" alt="" width= auto height= 140 >
-                   <h4 class="yellow-text "><b>Sede Jorge Elicer Gaitan</b></h4>
-                   <p class="green-text center" style="margin:5px 0;"><b>Vereda Regencia</b></p>
-               </div>
-           </div>
-       <br>
-       </div>
-   </div>
+                <!--- Sede La Bolsa --->
+                <div class="col s12 m6 center ">
+                    <img src="./images/mujer.png" alt="" width=auto height=140>
+                    <h4 class="yellow-text "><b>Sede Salvial</b></h4>
+                    <p class="green-text center" style="margin:5px 0;"><b>Vereda Salvial</b></p>
+                </div>
+
+                <!--- Sede El Rosal --->
+                <div class="col s12 m6 center ">
+                    <img src="./images/mujer.png" alt="" width=auto height=140>
+                    <h4 class="yellow-text "><b>Sede Jorge Elicer Gaitan</b></h4>
+                    <p class="green-text center" style="margin:5px 0;"><b>Vereda Regencia</b></p>
+                </div>
+            </div>
+            <br>
+        </div>
+    </div>
 </div>
 
 
@@ -54,8 +55,6 @@
     document.addEventListener("DOMContentLoaded", function() {
         document.body.style.backgroundImage = "url('./images/parallax/2.jpeg')";
     });
-
-   
 </script>
 
-<?php include('includes/footer.php');?>
+<?php include('includes/footer.php'); ?>
